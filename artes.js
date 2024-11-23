67b076de500f231224f468796df142722e886fd4
SonnicProgrammings <115489874+FeldsparGamer@users.noreply.github.com>
2024-11-23 19:37:09 -0300

    Update artes.js

--- artes.js
+++ artes.js
@@ -3,12 +3,15 @@ const arts = [
     { title: "Eniac", artist: "Liam Quezada", img: "artes/Arte2.png" },
     { title: "Max", artist: "Liam Quezada", img: "artes/Arte21.png" },
     { title: "Beta", artist: "Liam Quezada", img: "artes/Arte5.png" },
+    { title: "Alpha", artist: "Liam Quezada", img: "artes/Arte24.png" },
+    { title: "Mech", artist: "Liam Quezada", img: "artes/Arte25.png" },
     { title: "Trem", artist: "AdrielCrafter", img: "artes/Arte3.png" },
     { title: "Aniversário do Fupi", artist: "Sonnic1998", img: "artes/Arte4.png"},
     { title: "Marionic", artist: "Miguelitopira", img: "artes/Arte9.png"},
     { title: "Hollow Knight", artist: "Liam Quezada", img: "artes/Arte22.png" },
     { title: "Trem 2", artist: "AdrielCrafter", img: "artes/Arte15.png"},
     { title: "Prova Díficil", artist: "JohnmakerX", img: "artes/Arte6.png" },
+    { title: "Arte dos Animadores", artist: "Liam Quezada", img: "artes/Arte28.png" },
     { title: "EVA 01 e Shinji", artist: "Liam Quezada", img: "artes/Arte7.png"},
     { title: "Super Sonic RPG", artist: "JohnmakerX", img: "artes/Arte10.png"},
     { title: "Trem 3", artist: "AdrielCrafter", img: "artes/Arte13.png"},
@@ -18,9 +21,11 @@ const arts = [
     { title: "Família Românica", artist: "JohnmakerX", img: "artes/Arte17.png"},
     { title: "Arte de Halloween 2024", artist: "Liam Quezada", img: "artes/Arte20.png" },
     { title: "John e seus ídolos", artist: "JohnmakerX", img: "artes/Arte12.png"},
+    { title: "Halloween de Bot Planet", artist: "Liam Quezada", img: "artes/Arte26.png" },
     { title: "Estação Mogi das Cruzes", artist: "AdrielCrafter", img: "artes/Arte16.png"},
     { title: "Liam Quezada", artist: "Liam Quezada", img: "artes/Arte23.png" },
     { title: "Fogo Amigo", artist: "Sonnic1998", img: "artes/Arte18.png"},
+    { title: "Liam Vampiro", artist: "Liam Quezada", img: "artes/Arte27.png" },
     
   ];
   
